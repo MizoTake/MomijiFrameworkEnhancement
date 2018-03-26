1f3e3797b1802bafa9e5ab21a5daa8e7c47baf4d
MizoTake <mizotake123@gmail.com>
2018-03-27 01:43:20 +0900

    add postprocess & wire test scene contents

---
+++ Assets/Scripts/Behaviour/RandomScale.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Momiji;
+
+public class RandomScale : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _minScale;
+    [SerializeField]
+    private float _maxScale;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(RoopScale());
+    }
+
+    IEnumerator RoopScale()
+    {
+        while (true)
+        {
+            transform.DOScale(Vector3.zero.RandomRange(_minScale, _maxScale), 1.0f).Play();
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
