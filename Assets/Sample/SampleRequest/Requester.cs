5b3fdf3cd0777497b1d2d9451f1b42b77d8b7150
MizoTake <mizotake123@gmail.com>
2018-06-30 15:54:48 +0900

    Sample たたけた

---
+++ Assets/Sample/SampleRequest/Requester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Momiji.Sample;
+using UniRx;
+using UnityEngine;
+
+public class Requester : MonoBehaviour
+{
+
+	void Start ()
+	{
+		var param = new SampleParamter (city: 130010);
+		var request = new SampleGetRequest ();
+
+		request.Get (param)
+			.Subscribe (_ =>
+			{
+				Debug.Log (_.title);
+			})
+			.AddTo (this);
+	}
+}
