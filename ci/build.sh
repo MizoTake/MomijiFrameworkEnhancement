dbb35eacd5802e662d9439f564c98f54d9339cab
MizoTake <mizotake123@gmail.com>
2019-02-06 10:42:59 +0900

    change root directory

--- ci/build.sh
+++ ci/build.sh
@@ -21,7 +21,7 @@ xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
     -executeMethod BuildCommand.PerformBuild \
     -logFile \
     -nographics \
-    -manualLicenseFile ./Unity_v2018.ulf
+    -manualLicenseFile /Unity_v2018.ulf
 
 UNITY_EXIT_CODE=$?
 
